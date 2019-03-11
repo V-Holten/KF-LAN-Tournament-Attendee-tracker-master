7dbed829cfa4a8c87c856cd1e226292ec87088e2
Victor Heine Holten <vict1856@edu.eal.dk>
2019-03-11 14:21:29 +0100

    Add project files.

---
+++ KF-LAN Tournament Attendee tracker/Controller.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace KF_LAN_Tournament_Attendee_tracker
+{
+    public class Controller
+    {
+        public class Attendee
+        {
+            public string Name { get; set; }
+            public string Table { get; set; }
+            public string Seat { get; set; }
+            public string Tournament { get; set; }
+            public string Team { get; set; }
+
+        }
+
+        string connectionString = "Server=EALSQL1.eal.local; Database= A_DB31_2018; User Id=A_STUDENT31; Password=A_OPENDB31;";
+
+        public void AddAttendees(string Name, string Table, string Seat, string Tournament, string Team)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    SqlCommand AddAttendee = new SqlCommand("AddAttendee", connection)
+                    {
+                        CommandType = System.Data.CommandType.StoredProcedure
+                    };
+                    AddAttendee.Parameters.Add(new SqlParameter("@AttendeeName", Name));
+                    AddAttendee.Parameters.Add(new SqlParameter("@AttendeeTable", Table));
+                    AddAttendee.Parameters.Add(new SqlParameter("@AttendeeSeat", Seat));
+                    AddAttendee.Parameters.Add(new SqlParameter("@AttendeeTournament", Tournament));
+                    AddAttendee.Parameters.Add(new SqlParameter("@AttendeeTeam", Team));
+
+                    AddAttendee.ExecuteNonQuery();
+
+
+                }
+                catch (SqlException e)
+                {
+                    Console.WriteLine("Error: " + e.Message);
+                }
+            }
+        }
+
+        public void GetAttendees(MainWindow mainWindow)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    SqlCommand GetAttendees = new SqlCommand("GetAttendee", connection)
+                    {
+                        CommandType = System.Data.CommandType.StoredProcedure
+                    };
+
+                    SqlDataReader reader = GetAttendees.ExecuteReader();
+
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            string AttendeeName = reader["AttendeeName"].ToString();
+
+                            mainWindow.ListOfAttendees.Items.Add(new Attendee {
+                                Name = reader["AttendeeName"].ToString(),
+                                Table = reader["AttendeeTable"].ToString(),
+                                Seat = reader["AttendeeSeat"].ToString(),
+                                Team = reader["AttendeeTeam"].ToString(),
+                                Tournament = reader["AttendeeTournament"].ToString()
+                            });
+                        }
+                    }
+                }
+                catch (SqlException e)
+                {
+                    Console.WriteLine("Error: " + e.Message);
+                }
+            }
+        }
+
+        public void DeleteAttendee(string Name)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    SqlCommand DeleteAttendee = new SqlCommand("DeleteAttendee", connection)
+                    {
+                        CommandType = System.Data.CommandType.StoredProcedure
+                    };
+                    DeleteAttendee.Parameters.Add(new SqlParameter("@AttendeeName", Name));
+
+                    DeleteAttendee.ExecuteNonQuery();
+
+
+                }
+                catch (SqlException e)
+                {
+                    Console.WriteLine("Error: " + e.Message);
+                }
+            }
+        }
+    }
+}
---
+++ KF-LAN Tournament Attendee tracker/MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static KF_LAN_Tournament_Attendee_tracker.Controller;
+
+namespace KF_LAN_Tournament_Attendee_tracker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Controller controller = new Controller();
+        public MainWindow()
+        {
+            InitializeComponent();
+            controller.GetAttendees(this);
+        }
+
+        private void AddAttendeeButton_Click(object sender, RoutedEventArgs e)
+        {
+            controller.AddAttendees(NameBox.Text, TableBox.Text, SeatBox.Text, TournamentBox.Text, TeamBox.Text);
+            ListOfAttendees.Items.Add(new Attendee
+            {
+                Name = NameBox.Text,
+                Table = TableBox.Text,
+                Seat = SeatBox.Text,
+                Team = TeamBox.Text,
+                Tournament = TournamentBox.Text
+            });
+
+            NameBox.Text = "Name";
+            TableBox.Text = "Table";
+            SeatBox.Text = "Seat";
+            TournamentBox.Text = "Tournament";
+            TeamBox.Text = "Team";
+        }
+
+        private void DeleteAttendeeButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Attendee attendee = (Attendee)ListOfAttendees.SelectedItems[0];
+                controller.DeleteAttendee(attendee.Name);
+                ListOfAttendees.Items.RemoveAt(ListOfAttendees.SelectedIndex);
+            }
+            catch (System.ArgumentOutOfRangeException) { }
+        }
+        private void Box_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            TextBox box = sender as TextBox;
+            if (box.Text == box.Tag.ToString())
+            {
+                box.Text = "";
+            }
+        }
+
+        private void Box_LostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            TextBox box = sender as TextBox;
+            if (box.Text == "")
+            {
+                box.Text = box.Tag.ToString();
+            }
+        }
+
+        GridViewColumnHeader lastHeaderClicked = null;
+        ListSortDirection lastDirection = ListSortDirection.Ascending;
+
+        private void NameHeader_Click(object sender, RoutedEventArgs e)
+        {
+            GridViewColumnHeader column = (sender as GridViewColumnHeader);
+            ListSortDirection direction;
+            if (column != lastHeaderClicked)
+            {
+                direction = ListSortDirection.Ascending;
+            }
+            else
+            {
+                if (lastDirection == ListSortDirection.Ascending)
+                {
+                    direction = ListSortDirection.Descending;
+                }
+                else
+                {
+                    direction = ListSortDirection.Ascending;
+                }
+            }
+
+            string header = column.Tag.ToString();
+            ListOfAttendees.Items.SortDescriptions.Clear();
+            ListOfAttendees.Items.SortDescriptions.Add(new SortDescription(header, direction));
+
+            lastDirection = direction;
+            lastHeaderClicked = column;
+        }
+    }
+}
